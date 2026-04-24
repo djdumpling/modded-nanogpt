74c140343af2234069f907e6af8fa0062fef9904
djdumpling <alexwa2024@gmail.com>
2026-04-23 21:34:01 -0400

    num_extension_iterations from 40 to 18

--- train_gpt.py
+++ train_gpt.py
@@ -1577,7 +1577,7 @@ class Hyperparameters:
     val_batch_size: int = 4 * 64 * 1024 * 8
     # schedule
     num_scheduled_iterations: int = 1440  # number of steps to complete lr and ws schedule
-    num_extension_iterations: int = 40  # number of steps to continue training at final lr and ws
+    num_extension_iterations: int = 18  # number of steps to continue training at final lr and ws
     # evaluation and logging
     run_id: str = f"{uuid.uuid4()}"
     val_loss_every: int = 250  # every how many steps to evaluate val loss? 0 for only at the end
